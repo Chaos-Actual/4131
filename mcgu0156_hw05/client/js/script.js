dd45b6a4437b662f1ddbc7f120f6912f758837fc
Mark McGuire <mcgu0156@umn.edu>
2018-03-14 11:42:24 -0600

    Script.js file can dynamically update Favourite places table. Need to clean up input to remove special characters.

--- mcgu0156_hw05/client/js/script.js
+++ mcgu0156_hw05/client/js/script.js
@@ -1,9 +1,9 @@
 "use strict";
-console.log('Here');
+
 (function() {
 	// the API end point
 	var url = "getListOfFavPlaces";
-
+	var client = 'localhost:9007/places.json';
 	// TO DO: YOU NEED TO USE AJAX TO CALL getListOfFavPlaces end-point from server
 	// STEPS:
 	// 1. Hit the getListOfFavPlaces end-point of server using AJAX get method
@@ -11,27 +11,31 @@ console.log('Here');
 	// 2. Use the response returned to dynamically add rows to 'myFavTable' present in favourites.html page
 	// 3. You can make use of jQuery or JavaScript to achieve this
 	// Note: No changes will be needed in favourites.html page
+	$(document).ready(function(){
+    $.ajax({url: url, success: function(result){
+			var jsonString  = JSON.stringify(result);
 
-	console.log('Here');
-	$.ajax(
-	 {
-			 type: "GET",
-			 url: url,
-			 data: "{}",
-			 contentType: "application/json; charset=utf-8",
-			 dataType: "json",
-			 cache: false,
-			 success: function (data) {
-			 console.log(data);
-			 var trHTML = 'hello';
+			var length = result.res.placeList.length;
+			for (var i = 0; i < length; i++ ){
+				var trHTML = '<tr>';
+				var name = result.res.placeList[i].placename;
+				var address = result.res.placeList[i].addressline1 + result.res.placeList[i].addressline2;
+				var open_close = result.res.placeList[i].opentime + result.res.placeList[i].closetime;
+				var information = result.res.placeList[i].additionalinfo;
+				var link = result.res.placeList[i].additionalinfourl;
+				trHTML += '<th>'+name+'</th>';
+				trHTML += '<th>'+address+'</th>';
+				trHTML += '<th>'+open_close+'</th>';
+				trHTML += '<th>'+information+'</th>';
+				trHTML += '<th>'+link+'</th>';
+				trHTML += '</tr>';
+
+				$("#myFavTable").append(trHTML);
+			}
 
 
-			 $('#table').append(trHTML);
+  }});
+});
 
-			 },
 
-			 error: function (msg) {
-
-					 alert(msg.responseText);
-			 }
 })();
