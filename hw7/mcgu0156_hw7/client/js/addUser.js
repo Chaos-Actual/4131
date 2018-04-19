f6154d3b04f1b4b678dba29dfa8df390adf7b649
Mark McGuire <mcgu0156@umn.edu>
2018-04-19 13:34:28 -0500

    html try

--- hw7/mcgu0156_hw7/client/js/addUser.js
+++ hw7/mcgu0156_hw7/client/js/addUser.js
@@ -1,6 +1,9 @@
 function addUser() {
-
+	alert("here");
 	var user = '<tr><form name = "addUser" method = "post" action="/postUser"><td><input type="text" class="form-control" name="acc_name"  requiredmaxlength="15"></td><td><input type="text" class="form-control" name="acc_login" required maxlength="15"></td><td><input type="text" class="form-control" name="acc_password" required maxlength="15">/<td></tr>';
 
-		var element = document.getElementById("adminTable").append(user);
+		var element = document.getElementById("adminTable");
+		var row = element.insertRow();
+		row.innerHTML = user;
+		alert("here");
 };
